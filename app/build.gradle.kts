86e06618ec89e596689b6a92413fb3e4e32cfa03
Bamlax <bamlax@163.com>
2025-12-20 12:07:15 +0800

    0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.habitick"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.2"
+        versionCode = 7
+        versionName = "0.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
